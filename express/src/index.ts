f2626782cb363cd11d7a1325055d23e062a60f78
Lukas Feil <l.feil@oth-aw.de>
2023-04-27 21:49:16 +0200

    add express
    Former-commit-id: 159054c0f63da65de59014790e9518da4416ebc6

---
+++ express/src/index.ts
@@ -0,0 +1,32 @@
+import express, { Express, Request, Response } from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import router from './router/index.js';
+
+const app: Express = express();
+
+app.use(cors({ origin: true, credentials: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(compression());
+
+const server = http.createServer(app);
+
+server.listen(8080, () => {
+  console.log('Server is running on port 8080');
+});
+
+const MONGO_URL = "mongodb+srv://feillukas:"+encodeURIComponent("pEj?VQK>w2*2Uk4UAk5MS<9mDiT7fYyZ")+"@wae.6bsejdu.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (err: Error) => {
+  console.log(err);
+});
+
+app.use('/', router());
+
