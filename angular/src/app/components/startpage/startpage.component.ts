f8465bb2b7d0de2e37517582ae90885be1d58325
Lukas Feil <l.feil@oth-aw.de>
2023-05-02 13:49:41 +0200

    added startpage component
    Former-commit-id: b31600ae54c0c4c58f5e7c0ee6c94ec546f9e0e7

--- angular/src/app/components/startpage/startpage.component.ts
+++ angular/src/app/components/startpage/startpage.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-startpage',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterLink,
+    MatButtonModule
+  ],
   templateUrl: './startpage.component.html',
   styleUrls: ['./startpage.component.scss']
 })
