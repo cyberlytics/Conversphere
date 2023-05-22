2da499857d97221c40f76a28aa759e9dbb10b10c
Lukas Rupp <l.rupp@oth-aw.de>
2023-05-22 12:58:40 +0200

    changed stuff
    Former-commit-id: 33351d0c4934b06fb8aae7bb5343b4ae066631d6

--- angular/src/app/routes.ts
+++ angular/src/app/routes.ts
@@ -5,6 +5,7 @@ import { ErrorComponent } from './components/error/error.component';
 import { LebenslaufComponent } from './components/lebenslauf/lebenslauf.component';
 import { AuthComponent } from './components/login/auth/auth.component';
 import { LoginComponent } from './components/login/login.component';
+import { GameComponent } from './components/game/game.component';
 
 export const APP_ROUTES: Routes = [
     {
