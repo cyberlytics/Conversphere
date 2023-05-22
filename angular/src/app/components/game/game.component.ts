eb4d9d5a78644b297e9c4e7b2fa185c568d92f54
Lukas Rupp <l.rupp@oth-aw.de>
2023-05-22 15:34:49 +0200

    Added Slider
    Former-commit-id: 9c3a52472ffacadd55530d4c06d70320fe9e2302

--- angular/src/app/components/game/game.component.ts
+++ angular/src/app/components/game/game.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatSliderModule} from '@angular/material/slider';
 @Component({
   selector: 'app-game',
   standalone: true,
   imports: [
     CommonModule,
     MatButtonModule,
-    MatIconModule],
+    MatIconModule,
+    MatSliderModule],
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
