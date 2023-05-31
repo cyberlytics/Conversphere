9bb514885999e1fe9cf6891c393f1fc6223fed9c
Lukas Feil <l.feil@oth-aw.de>
2023-05-31 22:59:41 +0200

    use socker.io-client
    Former-commit-id: 9f342b8a4322cce710435703c034bf1da5837734

--- angular/src/app/services/chat.service.ts
+++ angular/src/app/services/chat.service.ts
@@ -1,16 +1,14 @@
 import { Injectable } from '@angular/core';
-import { Socket } from 'ngx-socket-io';
 import { map } from 'rxjs';
+import { io } from "socket.io-client";
+
 @Injectable({
   providedIn: 'root',
 })
 export class ChatService {
-  constructor(private socket: Socket) {}
+  socket = io("https://server-domain.com/chat");
 
-  sendMessage(msg: string) {
-    this.socket.emit('message', msg);
-  }
-  getMessage() {
-    return this.socket.fromEvent('message').pipe(map((data) => data.msg));
-  }
+  constructor() {}
+
+
 }
