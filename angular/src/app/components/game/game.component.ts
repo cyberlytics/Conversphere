ffe6e837315f50d95585b9f559afc1152c103c96
Lukas Rupp <l.rupp@oth-aw.de>
2023-06-16 09:03:12 +0200

    added some relative logic
    Former-commit-id: 41c4d2b6cad1358f92419f1dd48c15e21fcb8729

--- angular/src/app/components/game/game.component.ts
+++ angular/src/app/components/game/game.component.ts
@@ -12,6 +12,7 @@ import { AuthentificationService } from 'src/app/services/authentification.servi
 import { ChatService } from 'src/app/services/chat.service';
 import { User } from 'src/app/interfaces/users';
 import { Message } from 'src/app/interfaces/messages';
+import { Room } from 'src/app/interfaces/rooms';
 
 @Component({
   selector: 'app-game',
@@ -32,12 +33,18 @@ import { Message } from 'src/app/interfaces/messages';
 })
 export class GameComponent
 {
-  chatContent: Message[] | undefined = [{id:"Name", text:"Halloaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" , user_id:"500", visibility:1},{id:"Name 2", text:"Hallo2" , user_id:"501", visibility:1},{id:"Name 3", text:"Hallo 3" , user_id:"500", visibility:1},{id:"Name 4", text:"Hallo4" , user_id:"501", visibility:1},{id:"Name 5", text:"Hallo5" , user_id:"501", visibility:1},{id:"Name 6", text:"Hallo 6" , user_id:"500", visibility:1},{id:"Name 7", text:"Hallo8" , user_id:"501", visibility:1},{id:"Name 9", text:"Hallo9" , user_id:"501", visibility:1},{id:"Name 10", text:"Hallo 10" , user_id:"500", visibility:1},{id:"Name 11", text:"Hallo 11" , user_id:"500", visibility:1},{id:"Name 12", text:"Hallo12" , user_id:"501", visibility:1}];
-
+  chatContent: Message[] | undefined = [{id:"Name", text:"Halloaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" , user_id:"500", visibility:1},{id:"Name 2", text:"Hallo2" , user_id:"501", visibility:1},{id:"Name 3", text:"Hallo 3" , user_id:"500", visibility:1},{id:"Name 4", text:"Hallo4" , user_id:"501", visibility:1},{id:"Name 5", text:"Hallo5" , user_id:"501", visibility:1},{id:"Name 6", text:"Hallo 6" , user_id:"500", visibility:1},{id:"Name 7", text:"Hallo8" , user_id:"501", visibility:1},{id:"Name 9", text:"Hallo9" , user_id:"501", visibility:1},{id:"Name 10", text:"Hallo 10" , user_id:"500", visibility:1},{id:"Name 11", text:"Hallo 11" , user_id:"500", visibility:1},{id:"Name 12", text:"Hallo12" , user_id:"501", visibility:1}];
+  chatFontSize:number | undefined;
   user:User = {
     id: "500",
     nickname: "Testuser"
   }
+  rooms:Room={
+      id: "600",
+      name: "Raum 1",
+      description: "Das ist der Testraum 1"    
+  }
+
   constructor(private chatservice:ChatService)
   {
     chatservice.InitMessagesSocket().subscribe( (data) => {
@@ -61,7 +68,7 @@ export class GameComponent
   prozentualplayerheight=0;
   prozentualplayerwidth=0;
   player: HTMLElement | null | undefined;
-
+  chatmessage : HTMLElement | null | undefined;
   ngAfterInit(){
     this.player = document.getElementById("Spieler");
   }
@@ -100,6 +107,15 @@ export class GameComponent
       console.log((this.prozentualplayerwidth)+'px');
       this.player.style.top=(this.prozentualplayerheight*innerHeight)+'px';
     }
+    this.chatmessage=document.getElementById("chatnachricht");
+    if(this.chatmessage != null)
+    {
+      const windowwidth = window.innerWidth;
+      const windowheight = window.innerHeight;
+      this.chatmessage.style.fontSize=((Math.min(windowwidth,windowheight)/40)+'px');
+      //this.chatmessage.style.fontSize=(((windowwidth+windowheight)/120)+'px');
+    }
+    
   }
 
   formatlabel(value:number): string{
@@ -114,8 +130,6 @@ export class GameComponent
     }
     return '${value}';
   }
-
-
 
   messageControl = new FormControl('');
   sendMessage(){
