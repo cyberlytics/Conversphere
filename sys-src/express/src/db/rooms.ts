9dff35d25ae814a37e41eb14e8c8b71fa306998b
Sorel Tahata Djoumsi <s.tahata-djoumsi@oth-aw.de>
2023-06-17 18:01:45 +0200

    db updates
    Former-commit-id: dab4d2c0ac08c7e4f072afc45b37891ca451f7fb

--- sys-src/express/src/db/rooms.ts
+++ sys-src/express/src/db/rooms.ts
@@ -1,19 +1,26 @@
 import mongoose from "mongoose";
-
+import { getUserByNickname , createUser} from './users.js';
 const RoomScheme = new mongoose.Schema({
     id: {type: mongoose.Schema.Types.UUID, required: true},
     name: {type: String, required: true},
-    description: {type: String, required: true}
+    description: {type: String},
+    users: [{ type: String }]
 });
 
-export const RoomModel = mongoose.model("Room", RoomScheme);
 
-export const getRooms = () => RoomModel.find();
+export const Room = mongoose.model("Room", RoomScheme);
 
-export const leaveRoomWithId = (room_id: string, user_id: string) => {
+export const getRooms = () => Room.find();
+export const getRoomById = (id: string) => Room.findById(id);
+export const getRoomByName = (name: string) => Room.findOne({ name });
+export const getRoomByDescription = (description: string) => Room.findOne({ description });
+export const deleteRoomById = (id: string) => Room.findByIdAndDelete({ _id: id });
+export const createRoom = async (id: mongoose.Schema.Types.ObjectId, name: string, description: string) => new Room({ name, description }).save().then(room => room.toObject());
 
-}
+export const leaveRoomWithId = (room_id: string, user_id: string) => {Room.findByIdAndUpdate(room_id,{ $pull: { users: user_id } },{ new: true })};
 
-export const joinRoomWithNickname = (room_id: string, nickname: string) => {
-    
-}
+export const joinRoomWithNickname = (room_id: string, user_id: string) => {Room.findByIdAndUpdate(room_id,{ $addToSet: { users: user_id } },{ new: true })};
+
+export const getUsersByRoomId = async (room_id: string) => (await Room.findById(room_id))?.users;
+
+
--- sys-src/express/src/sockets/usersControllers.ts
+++ sys-src/express/src/sockets/usersControllers.ts
@@ -2,6 +2,9 @@ import { Namespace, Socket } from "socket.io";
 import { userDisconnected } from "./socketController.js";
 import { User } from "model/User.js";
 
+import { getUserByNickname , createUser} from '../db/users.js'; 
+import { getRooms , leaveRoomWithId, getUsersByRoomId} from '../db/rooms.js'; 
+import { getAllRooms } from "controllers/rooms.js";
 interface UserLeftMessage{
   userId: string
   nickname: string
@@ -10,22 +13,36 @@ interface UserLeftMessage{
 const connections: {[user_id: string]: Socket} = {}
 
 function handleUsersNamespace(nsp: Namespace) : void {
-  nsp.on("connection", (socket) => {
+  nsp.on("connection", (socket:any) => {
     const namespace = socket.nsp.name;
     if(/^\/ws\/rooms\/[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}\/users/.test(namespace)){
       // listen to events for user-actions (leave/join)
       if(typeof socket.handshake.query.user_id === 'string'){
         connections[socket.handshake.query.user_id] = socket;
-        socket.on("leaveRoom", (message: UserLeftMessage) => {
+        socket.on("leaveRoom", async (message: UserLeftMessage) => {
           console.log("User disconnected with user-id " + message.userId + " and nickname " + message.nickname);
+          
           // Get all the user for the room
-          // TODO -> replace with correct value
+          let room_id = "";
           const users:User[] = [];
+          const all_rooms=  await getRooms();// get all the rooms
+          for (const element of all_rooms) {//itterate through all the rooms
+            if ("users" in element && element.users.includes(message.nickname)) { // find the user
+              room_id=element.id; //get the id
+              break;
+            }
+
+          leaveRoomWithId(room_id,message.userId ); // the fonction throw the user and update the users list
+
+          let user_list=getUsersByRoomId(room_id);
+
+          /*
           users.push({id: 'asdfasdfasdfasdf', nickname: 'testUser1'})
           users.push({id: 'asdfasdfasdfasdf', nickname: 'testUser1'})
           users.push({id: 'asdfasdfasdfasdf', nickname: 'testUser1'})
           
           const user:User = {id: 'asdfasdf', nickname: 'asdf'}
+             */
 
           users.filter(x => x.id != message.userId).forEach((user)=>{
             const connection = connections[user.id];
