b3bbe1c6aabc7a8c7c0e3ce36f06c46a6e296f7e
Lukas Rupp <l.rupp@oth-aw.de>
2023-06-19 13:40:55 +0200

    added test
    Former-commit-id: 78e53764e8aef6382a2dfc78379a7b39535d6622

--- sys-src/angular/src/app/components/game/game.component.spec.ts
+++ sys-src/angular/src/app/components/game/game.component.spec.ts
@@ -1,4 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { GameComponent } from './game.component';
 
@@ -8,15 +14,39 @@ describe('GameComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [GameComponent],
+      declarations: [GameComponent],
+    imports: [
+      ReactiveFormsModule,
+      MatIconModule,
+      MatSliderModule,
+      MatSidenavModule,
+      MatToolbarModule,
+      RouterTestingModule,
+    ],
     }).compileComponents();
 
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(GameComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
+  it('Sollte Component erstellen', () => {
     expect(component).toBeTruthy();
+  });
+  
+  it('Sollte Wert von "opened" verändern wenn Button gedrückt wird', () => {
+    const initialOpenedValue = component.opened;
+    const button = fixture.nativeElement.querySelector('button[mat-icon-button]');
+    button.click();
+    expect(component.opened).toBe(!initialOpenedValue);
+  });
+  
+  it('Sollte Chatnachricht absenden wenn Button gedrückt wird', () => {
+    const message = 'Test';
+    component.messageControl.setValue(message);
+    const sendButton = fixture.nativeElement.querySelector('.buttonColor');
+    sendButton.click();
   });
 });
