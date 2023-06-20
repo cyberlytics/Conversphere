50d8198f1b5b7d684d3e46069e744c7d05cd2eeb
Stefan Reger <s.reger@oth-aw.de>
2023-06-20 12:13:17 +0200

    Former-commit-id: dbcdaa7609a365deab937f4a20371305777c9e03

--- sys-src/angular/src/app/components/startpage/startpage.component.ts
+++ sys-src/angular/src/app/components/startpage/startpage.component.ts
@@ -6,14 +6,12 @@ import {MatDialog, MatDialogModule} from '@angular/material/dialog';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {NgFor} from '@angular/common';
 import {MatListModule} from '@angular/material/list';
-
-//imports für List with single selection
+import { MatSnackBar } from '@angular/material/snack-bar';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-
-//imports für Select with form field
 import {MatInputModule} from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
 import {FormControl, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
@@ -41,7 +39,8 @@ interface Room {
     NgFor,FormsModule, 
     MatSelectModule, 
     MatInputModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatSnackBarModule,
   ],
 
   templateUrl: './startpage.component.html',
@@ -50,7 +49,7 @@ interface Room {
 
 
 export class StartpageComponent {
-
+  
   imagePath = 'assets/chatIcon.png';
   //List with single selection
   typesOfShoes: string[] = ["Raum1", "Raum2" , "Raum3", "Raum4"];
@@ -64,11 +63,14 @@ export class StartpageComponent {
   // Select with form field
   selectedValue: string | undefined;
   nicknameString!: string | null;
-  nickname = new FormControl('', [Validators.required, Validators.minLength(3)]);
+  nickname = new FormControl('', [Validators.required,Validators.required, Validators.pattern(/^\S*$/), Validators.minLength(3)]);
   opened: unknown;
   
-  constructor(public dialog: MatDialog) {}
+  constructor(public dialog: MatDialog, private snackBar: MatSnackBar, api-Service: ApiService) {
 
+  }
+
+  // Open Info Dialog for chattrules
   openDialog(): void {
     this.nicknameString = this.nickname.value;
     const dialogRef = this.dialog.open(UsernameComponent, {
@@ -77,6 +79,22 @@ export class StartpageComponent {
     });
     dialogRef.afterClosed().subscribe( () => {
       console.log('The dialog was closed');
+    });
+  }
+// Copy Link
+  linkToCopy = 'http://localhost:3000/start';
+
+  // Snackbar for Copy Link
+  copyToClipboard() {
+    const el = document.createElement('textarea');
+    el.value = this.linkToCopy;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+  
+    this.snackBar.open('Link wurde in die Zwischenablage kopiert', 'Schließen', {
+      duration: 4000,
     });
   }
 }
