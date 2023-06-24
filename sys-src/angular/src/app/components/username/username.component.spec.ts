6f12200529a1bfffcd516e1b866833bd61f33f6f
Stefan Reger <s.reger@oth-aw.de>
2023-06-24 09:50:42 +0200

    Former-commit-id: 699a637e9c5596cfffce298d9c7fd928842b4981

--- sys-src/angular/src/app/components/username/username.component.spec.ts
+++ sys-src/angular/src/app/components/username/username.component.spec.ts
@@ -1,21 +1,33 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import {provideHttpClientTesting }
+from '@angular/common/http/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { UsernameComponent } from './username.component';
 
-// import { UsernameComponent } from './username.component';
-
-// describe('UsernameComponent', () => {
-//   let component: UsernameComponent;
-//   let fixture: ComponentFixture<UsernameComponent>;
-
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       imports: [UsernameComponent]
-//     });
-//     fixture = TestBed.createComponent(UsernameComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
-
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+describe('UsernameComponent', () => {
+    let component: UsernameComponent;
+    let fixture: ComponentFixture<UsernameComponent>;
+    beforeEach(async () => {
+  
+      await TestBed.configureTestingModule({
+        imports: [ UsernameComponent, NoopAnimationsModule ],
+        providers: [
+          provideHttpClient(),
+          provideHttpClientTesting(),
+  
+          provideRouter([]),
+        ],
+      })
+      .compileComponents();
+  
+      fixture = TestBed.createComponent(UsernameComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+  
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+});  
